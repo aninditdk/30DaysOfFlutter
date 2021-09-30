80eb433106ff551b755559a76c844fd3d5e4751b
Anindit <aninditdk@gmail.com>
2021-09-30 12:58:59 +0530

    Day2 completed all

---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final num days = 30;
+    final String name = "Anindit";
+    return Scaffold(
+      appBar: AppBar(title: Text("Catalogue App")),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
